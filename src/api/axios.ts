50e4db2626626de196e44fc30c87f3f328fae926
chin <developer_02@jeweal.co.th>
2026-02-19 10:51:02 +0700

    update baseURL

--- src/api/axios.ts
+++ src/api/axios.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:5000/api",
+  baseURL: "https://cms-api-jn1c.onrender.com/api",
   headers: {
     "Content-Type": "application/json",
   },
